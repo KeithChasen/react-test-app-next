8053f036a5624d0dc2697fef59f2c5e6d2a823c7
keithchasenm <keithchasen89@gmail.com>
2019-01-10 13:27:43 +0200

    basic components

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    return(
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>
+                My name is Keith. And this is my test react app
+            </p>
+        </div>
+    )
+}
+
+export default About

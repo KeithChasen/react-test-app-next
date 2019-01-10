e731d940e547db557ecbae56a12684d03ec2117b
keithchasenm <keithchasen89@gmail.com>
2019-01-10 13:42:57 +0200

    basic router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-      </div>
+        <BrowserRouter>
+            <div className="App">
+                <Navbar />
+                <Route exact path='/' component={Home} />
+                <Route path='/about' component={About} />
+            </div>
+        </BrowserRouter>
     );
   }
 }

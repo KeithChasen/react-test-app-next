336cdb0c6a28fa102640ab42c4fda29b6f8674e4
keithchasenm <keithchasen89@gmail.com>
2019-01-11 13:50:22 +0200

    added image to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Rainbow from '../hoc/Rainbow'
+import Face from '../face.jpg'
 
 const About = () => {
     return(
-        <div className="container">
+        <div className="container about">
             <h4 className="center">About</h4>
+            <img src={Face} alt="Keith"/>
             <p>
                 My name is Keith. And this is my test react app
             </p>

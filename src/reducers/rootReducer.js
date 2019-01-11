706876a6fa6337339648d8c60441a042885900f7
keithchasenm <keithchasen89@gmail.com>
2019-01-11 15:53:35 +0200

    fetching posts from redux store

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-    posts: []
+    posts: [
+        {id:1,title:'Gereus uus turyed',body:'jsd asojd aosdjaidj asndmasndk jasdna sidn ai'},
+        {id:2,title:'Puruleous shturmudio',body:'jsd asojd aosdjaidj asndmasndk jasdna sidn ai'},
+        {id:3,title:'Zhbezhbezh strushio',body:'jsd asojd aosdjaidj asndmasndk jasdna sidn ai'},
+    ]
 }
 
 const rootReducer = (state = initState, action) => {
